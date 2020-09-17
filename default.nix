9034c549ead17a46ae65d1cb2ae3b1c938b7497f
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-18 00:51:04 +0200

    feat(default.nix): build bahnhofshalle using yarn2nix

--- default.nix
+++ default.nix
@@ -14,6 +14,15 @@ let
     filter = gi.gitignoreFilter (builtins.readFile ./.gitignore) root;
   };
 
+  yarn2nix = import (fetchFromGitHub {
+    owner  = "sternenseemann";
+    repo   = "yarn2nix";
+    rev    = "3811ffdc3b43a9ba8b8a7f7316399d8da90f8a38";
+    sha256 = "1r5f70qswrxzg0z4pk7v9l59aqnckrp70pxfplhyaq9vvys4f7l7";
+  }) { };
+
+  y2nlib = yarn2nix.passthru.nixLib;
+
   warteraumDrv = { stdenv, redo, scrypt }:
     stdenv.mkDerivation rec {
       pname = "warteraum";
@@ -59,6 +68,42 @@ rec {
     stdenv = clangStdenv;
     redo = redo-sh;
   };
+
+  bahnhofshalle =
+    let
+      deps = y2nlib.buildNodeDeps
+        (y2nlib.callYarnLock ./bahnhofshalle/yarn.lock { });
+      tpl = (y2nlib.callPackageJson ./bahnhofshalle/package.json { }) deps;
+      node_modules = y2nlib.linkNodeDeps {
+        dependencies = tpl.nodeBuildInputs;
+        inherit (tpl.key) name;
+      };
+    in stdenv.mkDerivation {
+      inherit (tpl) version;
+      inherit (tpl.key) name;
+
+      inherit src;
+      sourceRoot = sourceName + "/bahnhofshalle";
+
+      buildInputs = [ redo-sh ];
+
+      buildPhase = ''
+        # yeah … idk … node
+        ln -s "${node_modules}" node_modules
+        export NODE_PATH=node_modules
+
+        redo main.js
+      '';
+
+      installPhase = ''
+        install -Dm644 -t $out index.html
+        install -Dm644 -t $out openlab-logo.png
+        install -Dm644 -t $out main.js
+        install -Dm644 -t $out style.css
+      '';
+
+      dontStrip = true;
+    };
 
   pythonShell =
     let
