fa4e0689fc07401ef4717f4a8394c2c987ce503b
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-18 12:54:35 +0200

    feat(anzeigetafel): rename from flipper, adapt to api v2

--- anzeigetafel/anzeigetafel.py
+++ anzeigetafel/anzeigetafel.py
@@ -1,17 +1,23 @@
 import os
+import sys
 import requests
 from flipdots.scripts.scroll_text import scroll_text
 from time import sleep
 
-TOKEN = '7bf7303b847f359f32bff627519e4dd4f4bbc2d0638a758d81bbb156c5d30569'
-BASEURL = 'http://flipdot.openlab-augsburg.de/'
+if len(sys.argv) == 2:
+    TOKEN = sys.argv[1]
+else:
+    print("Usage: {} TOKEN".format(sys.argv[0]))
+    exit(1)
+
+BASEURL = 'http://flipdot.openlab-augsburg.de/api/v2'
 
 def get_queue():
     r = requests.get(BASEURL + '/queue')
     return r.json()['queue']
 
 def delete_queue_entry(id):
-    r = requests.delete(BASEURL + '/queue/del/{}'.format(id), data={'token' : TOKEN })
+    r = requests.delete(BASEURL + '/queue/{}'.format(id), data={'token' : TOKEN })
     if r.status_code == 204:
         return (True, r.status_code)
     else:
@@ -25,8 +31,8 @@ while True:
             queue = get_queue()
         except:
             pass
-        sleep(1)
-    
+        sleep(15)
+
     nextentry = queue[0]
     id = nextentry['id']
     text = nextentry['text']
