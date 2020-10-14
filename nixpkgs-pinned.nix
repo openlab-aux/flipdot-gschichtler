c411737010e85d41df2927a4c6a78dd0c50736f3
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-10-15 01:41:11 +0200

    chore(nixpkgs-pinned): use nixos/nixpkgs master
    TODO: as soon as changes hit nixos-unstable, it should probably be
          possible to disable the pin altogether.

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,6 +1,5 @@
 import (builtins.fetchTarball {
-  # nixpkgs master 2020-09-15 + scrypt with libscrypt-kdf
-  # pin should go to fork only temporarily
-  url = "https://github.com/sternenseemann/nixpkgs/archive/scrypt-1.3.1.tar.gz";
-  sha256 = "1knvly4w76wb5favmqzz2g4iihqcic757ly20lfxr14yr1ihcmhy";
+  # nixpkgs master 2020-10-14
+  url = "https://github.com/nixos/nixpkgs/archive/59a13254622de447eb638d1c6ff35e82d5c4e74b.tar.gz";
+  sha256 = "14db4ysc1mgmdbxlqyxdcj2q183g2wj0lh4jkrwdn5yi8chvg413";
 })
