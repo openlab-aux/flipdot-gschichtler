e68bdb5d01dd38461209772b9331345b21ceff16
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-06-14 13:31:15 +0200

    chore(nixpkgs-pinned): bump to nixos-unstable 2021-06-10

--- nix/nixpkgs-pinned.nix
+++ nix/nixpkgs-pinned.nix
@@ -1,5 +1,10 @@
+let
+  # nixos unstable 2021-06-10
+  commit = "432fc2d9a67f92e05438dff5fdc2b39d33f77997";
+  sha256 = "0npj480pxjzl5vjsbijsn275z48jpqpjp0q5xbwpr3fx6m8lcvr6";
+in
+
 import (builtins.fetchTarball {
-  # nixpkgs master 2021-04-17
-  url = "https://github.com/nixos/nixpkgs/archive/e5cc06a1e806070693add4f231060a62b962fc44.tar.gz";
-  sha256 = "04543i332fx9m7jf6167ac825s4qb8is0d0x0pz39il979mlc87v";
+  url = "https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz";
+  inherit sha256;
 })
