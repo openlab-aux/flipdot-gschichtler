5afd29a4fb85fe5ff0ac359613136ee7266e5630
sternenseemann <sternenseemann@systemli.org>
2025-08-11 13:20:20 +0200

    chore(warteraum): remove support for v1 API
    I could not find any evidence anyone has been using this API this
    year (except some AI companies' crawler bots sending GET requests to the
    wrong endpoints), so it seems like it's as good a time as any to pull
    the plug.
    
    The old endpoints now return 410 Gone. bahnhofshalle without JavaScript
    will now use the v2 API as a fallback, meaning that the user will be
    shown JSON instead of a (lackluster) HTML page.
    
    Due to the whitespace changes, it is easiest to review this change with
    `git diff -b`.

--- warteraum/v1_static.h
+++
@@ -1,5 +0,0 @@
-#define QUEUE_ADD_V1_SUCCESS \
-  "<!doctype html><head lang=\"en\"><meta charset=\"utf-8\"><title>success</title></head><body>your text was added to the queue</body></html>"
-
-#define QUEUE_ADD_V1_FAILURE \
-  "<!doctype html><head lang=\"en\"><meta charset=\"utf-8\"><title>failure</title></head><body>your request resulted in an error</body></html>"
